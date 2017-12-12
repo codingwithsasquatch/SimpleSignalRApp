4a30eaa1e1c436b2b74e8d63146b9c2286380486
David Barkol <dbarkol-c@convercent.com>
2017-12-11 17:50:24 -0800

    Initial revision

--- SimpleWeb/Startup.cs
+++ SimpleWeb/Startup.cs
@@ -22,6 +22,7 @@ namespace SimpleWeb
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +45,10 @@ namespace SimpleWeb
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+            });
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<NotificationsHub>("notifications");
             });
         }
     }
